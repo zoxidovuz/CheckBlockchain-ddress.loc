4f86e93eb5c39d23c494cdc112694502b1ed367b
zoxidovuz <umidjonzoxidov96@gmail.com>
2022-04-25 14:34:02 +0500

    BlackList

---
+++ app/Http/Middleware/BlockIpAddressMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class BlockIpAddressMiddleware
+{
+    /**
+     * @var string[]
+     */
+    public $blacklistIps = [
+        '127.0.0.1'
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (in_array($request->getClientIp(), $this->blacklistIps, true)) {
+            abort(403, "You are restricted to access the site.");
+        }
+
+        return $next($request);
+    }
+}
